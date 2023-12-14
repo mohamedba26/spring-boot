0eed9af870fedcb321a479b9fb45f9857ea78336
mohamedba26 <122885497+mohamedba26@users.noreply.github.com>
2023-12-14 10:05:14 +0100

    new

--- src/main/java/com/mohamed/springboot/repository/SalesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mohamed.springboot.repository;
-
-import com.mohamed.springboot.model.Sales;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesRepository extends JpaRepository<Sales,Long> {
-}
